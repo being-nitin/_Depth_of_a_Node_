b9dc9efbf2eb2c5ee2e117006f88878fa0115d8c
Nitin <nsinghboss02@gmail.com>
2022-03-21 17:07:16 +0530

    'printing nodes at depth k'

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -16,6 +16,21 @@ public class Main {
         node1.children.add(node4);
         node2.children.add(node5);
         node3.children.add(node6);
+        printAtK(root,2);
 
     }
+
+     public static void printAtK(Treenode<Integer> root,int k){
+        if(k<0){
+            return;
+        }
+        if(k==0){
+            System.out.println(root.data);
+        }
+        for(int i=0;i<root.children.size();i++){
+            printAtK(root.children.get(i),k-1);
+        }
+    }
+
+
 }
